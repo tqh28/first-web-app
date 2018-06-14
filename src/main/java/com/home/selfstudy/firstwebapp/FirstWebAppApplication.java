884d0dc2a510017d45efc42184d170cee89e078b
Huy Quang <tranquanghuy288@gmail.com>
2018-06-14 23:22:04 +0700

    .

--- src/main/java/com/home/selfstudy/firstwebapp/FirstWebAppApplication.java
+++ src/main/java/com/home/selfstudy/firstwebapp/FirstWebAppApplication.java
@@ -2,9 +2,10 @@ package com.home.selfstudy.firstwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FirstWebAppApplication {
+public class FirstWebAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstWebAppApplication.class, args);
